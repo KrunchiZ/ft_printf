34f7c877935e772088e674dfda77855861fce8d6
Kai Zhi Chiang <kchiang@u91z05s06.42kl.edu.my>
2025-06-06 16:40:40 +0800

    ft_printf wip. Current progress: ft_atoi precision

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 10:47:09 by kchiang           #+#    #+#             */
-/*   Updated: 2025/06/06 14:54:58 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/06/06 16:40:01 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ static t_fptr	fetch_pf(const char sp)
 	return (list[(int)sp]);
 }
 
+/* Using bits as boolean switches for the specifier flags 
+ * Zero-padding will be turn on only if there is no left align and precision
+ * */
 static void	parse_flag(const char c, t_spec *mod)
 {
 	mod->flag = 0;
@@ -38,12 +41,17 @@ static void	parse_flag(const char c, t_spec *mod)
 		mod->flag |= ALT_FORM;
 	else if (c == '.')
 		mod->flag |= HAS_PREC;
-	else if (c == '0' && !(mod->flag & LEFT_ALIGN) && !(mod->flag & HAS_PREC))
+	else if (c == '0')
 		mod->flag |= ZERO_PAD;
 	else if (c == '+')
 		mod->flag |= SHOW_SIGN;
-	else if (c == ' ' && !(mod->flag & SHOW_SIGN))
+	else if (c == ' ')
 		mod->flag |= ADD_SPACE;
+	if (mod->flag & ADD_SPACE && mod->flag & SHOW_SIGN)
+		mod->flag &= ~ADD_SPACE;
+	if (mod->flag & ZERO_PAD &&
+		((mod->flag & LEFT_ALIGN) || (mod->flag & HAS_PREC)))
+		mod->flag &= ~ZERO_PAD;
 	return ;
 }
 
@@ -52,7 +60,17 @@ static int	check_spec(const char **format, va_list ap, int len)
 	t_spec	mod;
 
 	while (ft_strchr(FLAG_SPEC, **format))
-		parse_flag(*(*format++), &mod);
+		parse_flag(*(*format)++, &mod);
+	if (ft_isdigit(**format))
+		mod.fdwidth = ft_atoi(*format);
+	while (ft_isdigit(**format))
+		(*format)++;
+	if (**format == '.')
+		parse_flag(*(*format)++, &mod);
+	if (ft_isdigit(**format))
+		mod.precision = ft_atoi(*format);
+	while (ft_isdigit(**format))
+		(*format)++;
 	return (len);
 }
 
