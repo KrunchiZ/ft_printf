c3cc3ee5300d5667da8225ce8116d336a0545a06
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-06-11 19:03:39 +0800

    len++ if is_neg is true for the '-' sign, while add is_neg check back into the SHOW_SIGN check so it only len++ for positive

--- pf_int_bonus.c
+++ pf_int_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:34:41 by kchiang           #+#    #+#             */
-/*   Updated: 2025/06/11 18:47:53 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/06/11 19:01:04 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ static char	*parse_int(t_llong nb, t_spec *mod, int *len)
 		if (!str)
 			return (NULL);
 	}
+	if (mod->is_neg)
+		(*len)++;
 	return (str);
 }
 
@@ -71,7 +73,7 @@ int	pf_int(va_list ap, t_spec mod)
 		str = parse_int(nb, &mod, &len);
 	if (!str)
 		return (-1);
-	if ((mod.flag & SHOW_SIGN) || (mod.flag & ADD_SPACE))
+	if (((mod.flag & SHOW_SIGN) && !(mod.is_neg)) || (mod.flag & ADD_SPACE))
 		len++;
 	if (mod.fdwidth < len)
 		mod.fdwidth = len;
